0a1d0b5c7c6efe2c57d8e53e539ad65c99f34ce8
macpre <macpre@macpreui-MacBook-Pro.local>
2018-06-06 20:07:49 +0900

    gemm add non-mutipliable case

--- hw6/hw6/hw6.cu
+++ hw6/hw6/hw6.cu
@@ -83,7 +83,7 @@ __global__ void gemm(float *a, float *b, float *c, const float alpha, const floa
         }
         __syncthreads();
         if(p == input_size/TILE_WIDTH - 1){
-            s_a[ty][tx] = alpha*a[row*input_size+(p+1)*TILE_WIDTH + tx]
+            s_a[ty][tx] = alpha*a[row*input_size+(p+1)*TILE_WIDTH + tx];
             s_b[ty][tx] = b[(p+1)*input_size*TILE_WIDTH+col+ty*input_size];
             __syncthreads();
             for(int i = 0; i < input_size%TILE_WIDTH; i++){
